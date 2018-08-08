23c35128b28901d62bfe44bb5f7cff007572da4b
z18podv <edouard.podvin.partner@decathlon.com>
2018-08-08 17:16:19 +0200

    Travis change in conf

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
 #    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "disquaire_project.settings")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "disquaire_project.__init__")
+#    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "disquaire_project.__init__")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
